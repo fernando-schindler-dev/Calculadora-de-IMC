8223afd80e0dae6c02ccaa2af4127a4df227b5c0
Fernando Schindler <fernando.schindler.dev@gmail.com>
2021-02-18 00:49:32 -0300

    Adicionado algumas melhorias no funcionamento

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const btnCalcular = document.querySelector(".btnCalcular");
-const resultado = document.querySelector(".resultado");
+const btnCalcular = document.querySelector('.btnCalcular');
+const resultado = document.querySelector('.resultado');
+
+function corrigirAltura(altura) {
+  let corrigido;
+
+  if (!(altura.includes(',') || altura.includes('.'))) {
+    const parte1 = altura.slice(0, 1);
+    const parte2 = altura.slice(1);
+    corrigido = parte1.concat('.', parte2);
+  }
+
+  return corrigido || altura;
+}
 
 function calculoIMC(peso, altura) {
-  const IMC = peso / (altura * altura);
+  const alturaCorrigida = corrigirAltura(altura);
+
+  const IMC = peso / (alturaCorrigida * alturaCorrigida);
+
   let resultIMC;
 
   if (IMC < 18.5) resultIMC = "<span class='red'>ABAIXO DO PESO</span>";
@@ -10,16 +25,21 @@ function calculoIMC(peso, altura) {
   else if (IMC < 29.9) resultIMC = "<span class='red'>SOBREPESO</span>";
   else if (IMC < 34.9) resultIMC = "<span class='red'>OBESIDADE GRAU I</span>";
   else if (IMC < 39.9) resultIMC = "<span class='red'>OBESIDADE GRAU II</span>";
-  else if (IMC > 40) resultIMC = "<span class='red'>OBESIDADE GRAU III</span>";
-  else resultIMC = "<span class='red'>POR FAVOR, PREENCHA OS CAMPOS!</span>";
+  else resultIMC = "<span class='red'>OBESIDADE GRAU III</span>";
 
   return resultIMC;
 }
 
-function gerarIMC() {
-  const peso = +document.querySelector(".peso").value;
-  const altura = +document.querySelector(".altura").value;
-  resultado.innerHTML = calculoIMC(peso, altura);
+function erroForm() {
+  return "<span class='erro'>POR FAVOR, PREENCHA OS CAMPOS!</span>";
 }
 
-btnCalcular.addEventListener("click", gerarIMC);
+function gerarIMC() {
+  const peso = +document.querySelector('.peso').value;
+  const altura = document.querySelector('.altura').value;
+
+  if (peso && altura) resultado.innerHTML = calculoIMC(peso, altura);
+  else resultado.innerHTML = erroForm();
+}
+
+btnCalcular.addEventListener('click', gerarIMC);
